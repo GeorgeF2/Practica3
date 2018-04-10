e66a12e8703700336257996b61cb17d670e155cb
Jorge Perez <alberto.perezf2@gmail.com>
2018-04-09 22:35:32 -0500

    Error at line 17 dont know why

--- Main.java
+++ Main.java
@@ -12,8 +12,10 @@ public class Main {
 
     State(int id, char[] a, int[] t, boolean f) {
       this.id = id;
-      for (int i = 0; i < a.length; i++)
+      for (int i = 0; i < a.length; i++){
+        System.out.println("for state " + id + " character " + a[i] + " goes to " + t[i]);
         this.trans.put(a[i], t[i]);
+      }
       this.fi = f;
     }
 
@@ -50,7 +52,26 @@ public class Main {
       int numStates = Integer.valueOf(temp);
       temp = JOptionPane.showInputDialog("whats the alphabet?");
       char[] alfabet = temp.toCharArray();
+      temp = JOptionPane.showInputDialog("what are the final states?");
+      char[] finalstates = temp.toCharArray();
 
+      for (int i = 0; i<numStates; i++) {
+        temp = JOptionPane.showInputDialog("For state " + i + " transitions");
+        char[] transitions = temp.toCharArray();
+        System.out.println("chararray = " + transitions[0] + ", "+ transitions[1]);
+        int[] tr = new int[temp.length()];
+        for (int j = 0; j<temp.length(); j++) {
+          tr[j] = Integer.valueOf(transitions[j]);
+        }
+        System.out.println("intarray = " + tr[0] + ", "+ tr[1]);
+        boolean check = false;
+        for (int j = 0; j<finalstates.length; j++) {
+          if (Integer.valueOf(finalstates[j]) == i){
+            check = true;
+          }
+        }
+        states[i] = new State(i, alfabet, tr, check);
+      }
     }
   }
 
